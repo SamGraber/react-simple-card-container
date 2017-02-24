47fefdefbdead3fbce72ebcb56277bb6ff33ca44
SamGraber <authorsamgraber@gmail.com>
2017-02-24 16:54:23 -0500

    Application setup
    React implementation of the ngx-simple-card-container I spiked. This is purely for my own enjoyment and as a comparison of the frameworks.

---
+++ app/app.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export const App = () => (
+	<div className="container">
+		<h1>Simple card container</h1>
+		{/*<scc-simple-card-container [data]="items"
+								[columns]="columns"
+								(sort)="sort($event)">
+			<div *sccCardContent="let myItem">
+				Name: {{myItem.name}}
+				Value: {{myItem.value}}
+			</div>
+			<div *sccCardFooter>Footer</div>
+		</scc-simple-card-container>*/}
+	</div>
+);
